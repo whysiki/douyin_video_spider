34993e97df6a18b7b41c6d7395786f651dde4e1e
whysiki <whysiki@proton.me>
2024-10-28 01:29:10 +0800

    11

--- main.py
+++ main.py
@@ -5,3 +5,9 @@ from playwright_dy import save_user_videos_aneme_jsonobjs
 user_home_page_urls = [
     "https://www.douyin.com/user/MS4wLjABAAAAnH5exW9sbuKNUVck8jWI6ajeA68coGy2fQ1lR5XOARk?from_tab_name=main"
 ]
+
+data_dir = "data"
+
+for user_home_page_url in user_home_page_urls:
+    save_user_videos_aneme_jsonobjs(user_home_page_url, data_save_dir=data_dir)
+    download_main(data_save_path=data_dir)
